98df9a6fd8a51a09b2df7916f2c235c4291478af
Amin Rahimzadeh <46243354+aminrz3@users.noreply.github.com>
2022-08-18 16:06:12 +0430

    Update dcgan_overriding_train_step.py

--- examples/generative/dcgan_overriding_train_step.py
+++ examples/generative/dcgan_overriding_train_step.py
@@ -1,13 +1,3 @@
-"""
-Title: DCGAN to generate face images
-Author: [fchollet](https://twitter.com/fchollet)
-Date created: 2019/04/29
-Last modified: 2021/01/01
-Description: A simple DCGAN trained using `fit()` by overriding `train_step` on CelebA images.
-"""
-"""
-## Setup
-"""
 
 import tensorflow as tf
 from tensorflow import keras
@@ -127,17 +117,17 @@ class GAN(keras.Model):
         return [self.d_loss_metric, self.g_loss_metric]
 
     def train_step(self, real_images):
-        # Sample random points in the latent space
+       
         batch_size = tf.shape(real_images)[0]
         random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
 
-        # Decode them to fake images
+    
         generated_images = self.generator(random_latent_vectors)
 
-        # Combine them with real images
+      
         combined_images = tf.concat([generated_images, real_images], axis=0)
 
-        # Assemble labels discriminating real from fake images
+       
         labels = tf.concat(
             [tf.ones((batch_size, 1)), tf.zeros((batch_size, 1))], axis=0
         )
@@ -153,21 +143,20 @@ class GAN(keras.Model):
             zip(grads, self.discriminator.trainable_weights)
         )
 
-        # Sample random points in the latent space
+        
         random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
 
-        # Assemble labels that say "all real images"
+      
         misleading_labels = tf.zeros((batch_size, 1))
 
-        # Train the generator (note that we should *not* update the weights
-        # of the discriminator)!
+        
         with tf.GradientTape() as tape:
             predictions = self.discriminator(self.generator(random_latent_vectors))
             g_loss = self.loss_fn(misleading_labels, predictions)
         grads = tape.gradient(g_loss, self.generator.trainable_weights)
         self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
 
-        # Update metrics
+     
         self.d_loss_metric.update_state(d_loss)
         self.g_loss_metric.update_state(g_loss)
         return {
@@ -200,7 +189,7 @@ class GANMonitor(keras.callbacks.Callback):
 ## Train the end-to-end model
 """
 
-epochs = 1  # In practice, use ~100 epochs
+epochs = 1 
 
 gan = GAN(discriminator=discriminator, generator=generator, latent_dim=latent_dim)
 gan.compile(
